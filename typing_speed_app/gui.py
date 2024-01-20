6d95c741001655d6c220dfae8c5ac6bc88b84374
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2024-01-20 12:08:45 +0100

    Insert random words

--- typing_speed_app/gui.py
+++ typing_speed_app/gui.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from words_generator import generate_words
 
 
 class AppWindow(tk.Tk):
-    def __init__(self, container):
+    def __init__(self):
         super().__init__()
 
         self.title("Typing Speed Test")
@@ -12,3 +13,26 @@ class AppWindow(tk.Tk):
 
     def __repr__(self) -> str:
         return f"Subclass {AppWindow} of {super().__repr__()}"
+
+
+class MainFrame(ttk.Frame):
+    WORDS_PER_ROUND = 5
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.bank_text_display = tk.Text(
+            master=parent, wrap="word", width=50, height=10
+        )
+        self.bank_text_display.grid(row=0, column=0)
+        self.bank_text_display.insert(index="1.0", chars=self.get_random_words())
+
+    @staticmethod
+    def get_random_words(words=WORDS_PER_ROUND):
+        return generate_words(words)
+
+
+# debug
+app = AppWindow()
+MainFrame(parent=app)
+app.mainloop()
